35cca5bdd8850df09439083c5f6c9bee149f81bb
Luay Nasr <luayonasr@gmail.com>
2024-05-27 12:59:14 -0400

    0.4

--- Admin/admin.py
+++ Admin/admin.py
@@ -1,19 +1,19 @@
-import streamlit as st
-import boto3
-import os
-import uuid
-from langchain_community.embeddings import BedrockEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.vectorstores import FAISS
+import streamlit as st  # Streamlit for creating the web interface
+import boto3  # AWS SDK for Python
+import os  # For environment variables
+import uuid  # For generating unique identifiers
+from langchain_community.embeddings import BedrockEmbeddings  # Embeddings from LangChain community
+from langchain.text_splitter import RecursiveCharacterTextSplitter  # Text splitter from LangChain
+from langchain_community.document_loaders import PyPDFLoader  # PDF loader from LangChain community
+from langchain_community.vectorstores import FAISS  # Vector store implementation
 
 # AWS Configuration
-AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
-bedrock_client = boto3.client(service_name="bedrock-runtime", region_name=AWS_REGION)
-bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock_client)
-s3_client = boto3.client("s3", region_name=AWS_REGION)
-BUCKET_NAME = os.getenv("BUCKET_NAME")
-folder_path = "/tmp/"
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")  # Get AWS region from environment variable or default to 'us-east-1'
+bedrock_client = boto3.client(service_name="bedrock-runtime", region_name=AWS_REGION)  # Initialize Bedrock client
+bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock_client)  # Initialize Bedrock embeddings
+s3_client = boto3.client("s3", region_name=AWS_REGION)  # Initialize S3 client
+BUCKET_NAME = os.getenv("BUCKET_NAME")  # Get S3 bucket name from environment variable
+folder_path = "/tmp/"  # Local folder path for temporary file storage (AWS)
 
 def get_unique_id():
     """Generate a unique identifier."""
@@ -21,85 +21,94 @@ def get_unique_id():
 
 def split_text(pages, chunk_size=1000, chunk_overlap=200):
     """Split the text into manageable chunks."""
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    return text_splitter.split_documents(pages)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)  # Initialize text splitter
+    return text_splitter.split_documents(pages)  # Split the document into chunks
 
 def create_vector_store(request_id, documents):
     """Create and upload a vector store to S3 for admin use."""
     try:
+        # Create a FAISS vector store from the documents
         vectorstore_faiss = FAISS.from_documents(documents, bedrock_embeddings)
-        file_name = f"{request_id}.bin"
-        vectorstore_faiss.save_local(index_name=file_name, folder_path=folder_path)
+        file_name = f"{request_id}.bin"  # Generate the file name
+        vectorstore_faiss.save_local(index_name=file_name, folder_path=folder_path)  # Save vector store locally
+        
+        # Upload the FAISS index and metadata files to S3
         s3_client.upload_file(Filename=f"{folder_path}{file_name}.faiss", Bucket=BUCKET_NAME, Key=f"{request_id}.faiss")
         s3_client.upload_file(Filename=f"{folder_path}{file_name}.pkl", Bucket=BUCKET_NAME, Key=f"{request_id}.pkl")
         return True
     except Exception as e:
-        st.error(f"Error creating vector store: {e}")
+        st.error(f"Error creating vector store: {e}")  # Display error message
         return False
 
 def get_latest_object_key(bucket_name):
     """Retrieve the key of the latest object in the specified S3 bucket."""
     try:
-        s3 = boto3.client('s3')
-        response = s3.list_objects_v2(Bucket=bucket_name)
+        s3 = boto3.client('s3')  # Initialize S3 client
+        response = s3.list_objects_v2(Bucket=bucket_name)  # List objects in the specified bucket
 
-        latest_object = None
-        for obj in response.get('Contents', []):
+        latest_object = None  # Initialize a variable to keep track of the latest object
+        for obj in response.get('Contents', []):  # Iterate through the objects
             if latest_object is None or obj['LastModified'] > latest_object['LastModified']:
-                latest_object = obj
+                latest_object = obj  # Update the latest object
 
         if latest_object:
-            return latest_object['Key']
+            return latest_object['Key']  # Return the key of the latest object
         else:
-            st.error("Bucket is empty or no objects found.")
+            st.error("Bucket is empty or no objects found.")  # Display error message if no objects found
             return None
     except Exception as e:
-        st.error(f"Error retrieving latest object from S3: {e}")
+        st.error(f"Error retrieving latest object from S3: {e}")  # Display error message
         return None
 
 def admin_interface():
     """Admin interface to manage document processing and vector store management."""
-    st.title("Admin Dashboard for Document Management")
+    st.title("Admin Dashboard for Knowledgebase Management")  # Set the title of the web interface
 
+    # Display the latest object key in S3
     latest_key = get_latest_object_key(BUCKET_NAME)
     if latest_key:
         st.write(f"Latest object key in S3: {latest_key}")
 
+    # File uploader for document processing
     uploaded_file = st.file_uploader("Upload a document for processing", type=["pdf", "txt"])
     if uploaded_file is not None:
         try:
-            request_id = str(uuid.uuid4())
+            request_id = get_unique_id()  # Generate a unique request ID
             st.write(f"Request ID: {request_id}")
-            file_extension = "pdf" if uploaded_file.type == "application/pdf" else "txt"
-            saved_file_name = f"{request_id}.{file_extension}"
+            file_extension = "pdf" if uploaded_file.type == "application/pdf" else "txt"  # Determine file extension
+            saved_file_name = f"{request_id}.{file_extension}"  # Generate the saved file name
             
+            # Save uploaded file locally
             with open(saved_file_name, "wb") as file:
                 file.write(uploaded_file.getvalue())
 
+            # Load and split document based on file type
             if file_extension == "pdf":
-                loader = PyPDFLoader(saved_file_name)
-                pages = loader.load_and_split()
+                loader = PyPDFLoader(saved_file_name)  # Initialize PDF loader
+                pages = loader.load_and_split()  # Load and split PDF document
             else:
                 with open(saved_file_name, "r") as file:
-                    pages = file.read().split('\n\n')
+                    pages = file.read().split('\n\n')  # Split text document into pages
 
             st.write(f"Total Pages: {len(pages)}")
-            splitted_docs = split_text(pages)
+            splitted_docs = split_text(pages)  # Split the document into chunks
             st.write(f"Splitted Docs length: {len(splitted_docs)}")
 
+            # Button to create vector store
             if st.button("Create Vector Store"):
-                result = create_vector_store(request_id, splitted_docs)
+                result = create_vector_store(request_id, splitted_docs)  # Create and upload vector store
                 if result:
                     st.success("Vector store created and uploaded successfully.")
                 else:
                     st.error("Failed to create vector store. Check logs for details.")
+
                 # Update the latest object key in S3 after creating the vector store
                 latest_key = get_latest_object_key(BUCKET_NAME)
                 if latest_key:
                     st.write(f"Updated latest object key in S3: {latest_key}")
 
         except Exception as e:
-            st.error(f"An error occurred: {e}")
+            st.error(f"An error occurred: {e}")  # Display error message
 
 if __name__ == "__main__":
-    admin_interface()
+    admin_interface()  # Run the admin interface
--- User/app.py
+++ User/app.py
@@ -1,55 +1,53 @@
-import boto3
-import streamlit as st
-import os
-import uuid
-from langchain_community.embeddings import BedrockEmbeddings
-from langchain_community.llms import Bedrock  # Update import based on deprecation warning
-from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
-from langchain_community.vectorstores import FAISS
-from datetime import datetime
+import boto3  # AWS SDK for Python
+import streamlit as st  # Streamlit for web app
+import os  # For environment variables
+import uuid  # For unique identifier generation
+from langchain_community.embeddings import BedrockEmbeddings  # Embeddings from LangChain community
+from langchain_community.llms import Bedrock  # Large Language Model from LangChain community
+from langchain.prompts import PromptTemplate  # Template for creating prompts
+from langchain.chains import RetrievalQA  # Chain for retrieval-based QA
+from langchain_community.vectorstores import FAISS  # Vector store implementation
+from datetime import datetime  # For handling date and time
 
 # AWS and Bedrock configuration
-AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
-bedrock_client = boto3.client(service_name="bedrock-runtime", region_name=AWS_REGION)
-bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock_client)
-s3_client = boto3.client("s3", region_name=AWS_REGION)
-BUCKET_NAME = os.getenv("BUCKET_NAME")
-folder_path = "/tmp/"
-
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")  # Get AWS region from environment variable or default to 'us-east-1'
+bedrock_client = boto3.client(service_name="bedrock-runtime", region_name=AWS_REGION)  # Initialize Bedrock client
+bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock_client)  # Initialize Bedrock embeddings
+s3_client = boto3.client("s3", region_name=AWS_REGION)  # Initialize S3 client
+BUCKET_NAME = os.getenv("BUCKET_NAME")  # Get S3 bucket name from environment variable
+folder_path = "/tmp/"  # Local folder path for temporary file storage (AWS)
 
 def load_latest_faiss():
-    # List objects in the specified bucket
+    """Load the latest FAISS file from the specified S3 bucket."""
     print(f"{BUCKET_NAME}")
-    response = s3_client.list_objects_v2(Bucket=BUCKET_NAME)
-    # Initialize a variable to keep track of the latest object
-    latest_object = None
+    response = s3_client.list_objects_v2(Bucket=BUCKET_NAME)  # List objects in the specified bucket
+    latest_object = None  # Initialize a variable to keep track of the latest object
 
     # Iterate through the objects and find the latest one
     for obj in response.get('Contents', []):
         if latest_object is None or obj['LastModified'] > latest_object['LastModified']:
             latest_object = obj
     if latest_object:
-       return latest_object['Key']
+        return latest_object['Key']  # Return the key of the latest object
     else:
-        return None
+        return None  # Return None if no objects found
 
-    
 def load_index(FAISS_file_name):
     """Download vector store files from S3 to local."""
     print(FAISS_file_name)
+    # Download the FAISS and associated metadata files from S3 to local folder
     s3_client.download_file(Bucket=BUCKET_NAME, Key=f"{FAISS_file_name}.faiss", Filename=f"{folder_path}{FAISS_file_name}.faiss")
     s3_client.download_file(Bucket=BUCKET_NAME, Key=f"{FAISS_file_name}.pkl", Filename=f"{folder_path}{FAISS_file_name}.pkl")
 
-
-
 def get_llm(model_id):
     """Initialize and return a Bedrock Large Language Model."""
+    # Create an instance of the Bedrock LLM with specified model ID and parameters
     llm = Bedrock(model_id=model_id, client=bedrock_client, model_kwargs={'max_tokens_to_sample': 512})
     return llm
 
 def get_response(llm, vectorstore, question, prompt_style="zero-shot"):
     """Generate a response for a given question using a specified LLM and prompt style."""
+    # Define the prompt template based on the selected style
     prompt_template = {
         "zero-shot": """
         Human: Provide a concise answer to the question using the given context.
@@ -63,7 +61,9 @@ def get_response(llm, vectorstore, question, prompt_style="zero-shot"):
         Assistant:"""
     }[prompt_style]
 
+    # Create a prompt template
     PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    # Initialize the RetrievalQA chain
     qa = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",  # Define the appropriate chain type
@@ -72,16 +72,20 @@ def get_response(llm, vectorstore, question, prompt_style="zero-shot"):
         chain_type_kwargs={"prompt": PROMPT}
     )
 
+    # Get the response from the QA chain
     answer = qa({"query": question})
     return answer['result']
 
 def main():
     """Main function to run the Streamlit app for a utility chatbot."""
     st.header("Avertra Utility Chat Bot")
+    # Load the latest FAISS file from S3
     FAISS_file_name = load_latest_faiss().split('.faiss')[0]
+    # Download the index files
     load_index(FAISS_file_name)
     st.write(f"Knowledge Base in: {FAISS_file_name}")
     
+    # Load the FAISS index from local files
     faiss_index = FAISS.load_local(
         index_name=f"{FAISS_file_name}",
         folder_path=folder_path,
@@ -90,20 +94,23 @@ def main():
     )
 
     st.write("INDEX IS READY")
+    # Input field for user question
     question = st.text_input("Please ask your question")
 
+    # Options for selecting model and prompt style
     model_options = ["anthropic.claude-v2:1"]
     prompt_styles = ["zero-shot", "few-shot"]
 
     selected_model = st.selectbox("Choose the model", model_options)
     selected_prompt_style = st.selectbox("Choose the prompt style", prompt_styles)
 
+    # Button to ask the question
     if st.button("Ask Question"):
         with st.spinner("Querying..."):
-            llm = get_llm(selected_model)
-            response = get_response(llm, faiss_index, question, selected_prompt_style)
-            st.write(response)
+            llm = get_llm(selected_model)  # Initialize the LLM
+            response = get_response(llm, faiss_index, question, selected_prompt_style)  # Get the response
+            st.write(response)  # Display the response
             st.success("Done")
 
 if __name__ == "__main__":
-    main()
+    main()  # Run the main function
